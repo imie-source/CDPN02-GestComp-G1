4e3acf22f7642d53b492f3f5d7c3a35be1b0246f
sberry42 <sarahberry42@gmail.com>
2013-12-20 14:04:39 +0100

    Service notification

--- skills_service_impl/src/fr/skills/service/impl/NotificationService.java
+++ skills_service_impl/src/fr/skills/service/impl/NotificationService.java
@@ -1,5 +1,7 @@
 package fr.skills.service.impl;
 
+import java.util.List;
+
 import fr.skills.dao.inter.DAO;
 import fr.skills.dao.inter.FactoryDAO;
 import fr.skills.dto.NotificationDTO;
@@ -8,14 +10,79 @@ import fr.skills.service.inter.INotificationService;
 public class NotificationService implements INotificationService {
 	private final DAO<NotificationDTO> notifDAO;
 
-	public NotificationService(FactoryDAO afdao)
-	{
+	public NotificationService(FactoryDAO afdao) {
 		notifDAO = afdao.getNotificationDAO();
 	}
 
-	public DAO<NotificationDTO> getNotifDAO()
-	{
-		return notifDAO;
+	@Override
+	public NotificationDTO create(NotificationDTO aNotifDTO) {
+		if (isOK("create", aNotifDTO)) {
+			aNotifDTO = notifDAO.create(aNotifDTO);
+		}
+		return aNotifDTO;
+	}
+
+	@Override
+	public Boolean delete(NotificationDTO aNotifDTO) {
+		Boolean vRet = false;
+		if (isOK("delete", aNotifDTO)) {
+			vRet = notifDAO.delete(aNotifDTO);
+		}
+		return vRet;
+	}
+
+	@Override
+	public Boolean update(NotificationDTO aNotifDTO) {
+		Boolean vRet = false;
+		if (isOK("update", aNotifDTO)) {
+			notifDAO.update(aNotifDTO);
+		}
+		return vRet;
+	}
+
+	@Override
+	public NotificationDTO find(Integer anId) {
+		NotificationDTO vRet = null;
+		if (anId != null) {
+			vRet = notifDAO.find(anId);
+		}
+		return vRet;
+	}
+
+	@Override
+	public List<NotificationDTO> findAll(Integer anId) {
+		return notifDAO.findAll();
+	}
+
+	@Override
+	public Boolean isOK(String anAction, final NotificationDTO aNotifDTO) {
+		Boolean vRet = false;
+		if (aNotifDTO != null) {
+			switch (anAction.toLowerCase()) {
+			case "create":
+				if ((aNotifDTO.getDateNotification() != null)
+						&& (aNotifDTO.getNotification() != null)) {
+					vRet = true;
+				}
+				break;
+			case "delete":
+				if (aNotifDTO.getIdNotification() != null) {
+					vRet = true;
+				}
+				break;
+
+			case "update":
+				if ((aNotifDTO.getIdNotification() != null)
+						&& (aNotifDTO.getDateNotification() != null)
+						&& (aNotifDTO.getNotification() != null)) {
+					vRet = true;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		return vRet;
 	}
 
 }
--- skills_service_int/src/fr/skills/service/inter/INotificationService.java
+++ skills_service_int/src/fr/skills/service/inter/INotificationService.java
@@ -1,5 +1,19 @@
 package fr.skills.service.inter;
 
-public interface INotificationService {
+import java.util.List;
 
+import fr.skills.dto.NotificationDTO;
+
+public interface INotificationService {
+	public NotificationDTO create(NotificationDTO aNotifDTO);
+
+	public Boolean delete(NotificationDTO aNotifDTO);
+
+	public Boolean update(NotificationDTO aNotifDTO);
+
+	public NotificationDTO find(Integer anId);
+
+	public List<NotificationDTO> findAll(Integer anId);
+
+	public Boolean isOK(String anAction, final NotificationDTO aNotifDTO);
 }
