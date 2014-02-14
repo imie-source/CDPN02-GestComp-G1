836c66ab0783ffa7d36bdf056460d5371ced4658
Bluemriine <remi.legourrierec@gmail.com>
2014-02-14 11:42:11 +0100

    Modif JPA

--- skills_jpa_impl_temp/src/fr/skills/jpa/impl/temp/ServletTestJPA1.java
+++ skills_jpa_impl_temp/src/fr/skills/jpa/impl/temp/ServletTestJPA1.java
@@ -34,7 +34,7 @@ public class ServletTestJPA1 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MaBaseDeTestPU");   
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("skills_jpa");   
 		EntityManager em = emf.createEntityManager();    
 		
 		System.out.println("test");
