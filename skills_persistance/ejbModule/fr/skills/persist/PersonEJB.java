7dd47c4370b7caed058d013ebdc3871676aca5b6
sberry42 <sarahberry42@gmail.com>
2014-12-09 14:03:28 +0100

    ajout persistance

---
+++ skills_persistance/ejbModule/fr/skills/persist/PersonEJB.java
@@ -0,0 +1,48 @@
+package fr.skills.persist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import fr.skills.jpa.entities.Person;
+
+/**
+ * Session Bean implementation class PersonEJB
+ */
+@Stateless
+@LocalBean
+public class PersonEJB {
+	@PersistenceContext
+	EntityManager em;
+
+	/**
+	 * Default constructor.
+	 */
+	public PersonEJB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person findById(int anId) {
+		Person vRet = em.find(Person.class, anId);
+
+		return vRet;
+	}
+
+	public void update(Person aPerson) {
+		Person vRet = em.merge(aPerson);
+		save(vRet);
+	}
+
+	public void save(Person aPerson) {
+		em.persist(aPerson);
+	}
+
+	public List<Person> findAll() {
+		return (List<Person>) em.find(Person.class, null);
+	}
+
+}
