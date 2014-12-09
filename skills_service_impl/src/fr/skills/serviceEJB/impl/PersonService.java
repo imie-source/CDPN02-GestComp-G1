2f6fcf99ec9fd5af305be1835d55bccdb7121a9d
sberry42 <sarahberry42@gmail.com>
2014-12-09 12:19:22 +0100

    couche métier + refactor

--- skills_service_impl/src/fr/skills/serviceEJB/impl/PersonService.java
+++ skills_service_impl/src/fr/skills/serviceEJB/impl/PersonService.java
@@ -2,23 +2,25 @@ package fr.skills.serviceEJB.impl;
 
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import fr.skills.jpa.entities.Person;
+import fr.skills.persist.PersonEJB;
 import fr.skills.serviceEJB.inter.IPersonService;
 
 @Stateless
 public class PersonService implements IPersonService{
 
-	@PersistenceContext
-	EntityManager em;
+	@EJB
+	private PersonEJB person;
+	
 	@Override
 	public List<Person> getAll() {
 		// TODO Auto-generated method stub
-		
-		return (List<Person>) em.createNamedQuery("Player.findAll").getResultList();
+		return person.findAll();
 	}
 
 }
